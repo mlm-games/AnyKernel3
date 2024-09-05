510327a42ad9f89f26a9311914596f2b1f67bdf9
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2024-09-05 11:19:26 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Openela_rusk Kernel by osm0sis @ xda-developers
+kernel.string=Openela_rusk Kernel by ragebreaker
 do.devicecheck=1
 do.modules=0
 do.systemless=1
