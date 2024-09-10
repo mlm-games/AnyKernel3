6f8ebb77c390c5fd9e14a96c44677a59193af6e8
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2024-09-10 20:52:04 +0530

    Update kernel_zipper.sh to check out/

--- kernel_zipper.sh
+++ kernel_zipper.sh
@@ -10,57 +10,50 @@ FINAL_KERNEL_ZIP="RuskKernel.zip"
 git clone "$ANYKERNEL_REPO" "$ANYKERNEL_DIR" --depth=1
 
 # Function to find and copy kernel image
-copy_kernel_image() {
-    local boot_dir="$KERNEL_SOURCE_DIR/arch"
-    local kernel_image=""
+find_and_copy_kernel() {
+    local search_dirs=("arch/arm/boot" "arch/arm64/boot" "out/arch/arm/boot" "out/arch/arm64/boot")
+    local kernel_names=("zImage-dtb" "Image.gz-dtb" "Image.gz"  "Image" "kernel")
 
-    for arch in arm arm64; do
-        if [ -d "$boot_dir/$arch/boot" ]; then
-            # Search for kernel images in order of preference
-            for img in zImage-dtb Image.gz-dtb Image.gz Image kernel; do
-                kernel_image=$(find "$boot_dir/$arch/boot" -name "$img" | head -n 1)
-                if [ -n "$kernel_image" ]; then
-                    cp "$kernel_image" "$ANYKERNEL_DIR/"
-                    echo "Copied kernel image: $kernel_image"
-                    return 0
-                fi
-            done
-        fi
+    for dir in "${search_dirs[@]}"; do
+        for name in "${kernel_names[@]}"; do
+            if [ -f "$KERNEL_DIR/$dir/$name" ]; then
+                cp "$KERNEL_DIR/$dir/$name" "$ANYKERNEL_DIR/"
+                echo "Copied kernel: $dir/$name"
+                return 0
+            fi
+        done
     done
 
-    echo "No kernel image found"
+    echo "No kernel image found!"
     return 1
 }
 
-# Function to find and copy one DTB/DTBO file
-copy_dtb_dtbo() {
-    local boot_dir="$KERNEL_SOURCE_DIR/arch"
-    local dtb_file=""
+# Function to find and copy DTB/DTBO
+find_and_copy_dtb_dtbo() {
+    local search_dirs=("arch/arm/boot/dts" "arch/arm64/boot/dts" "out/arch/arm/boot/dts" "out/arch/arm64/boot/dts")
+    local dtb_patterns=("*.dtb" "*.dtbo")
 
-    for arch in arm arm64; do
-        if [ -d "$boot_dir/$arch/boot/dts" ]; then
-            # Search for DTB/DTBO files
-            dtb_file=$(find "$boot_dir/$arch/boot/dts" -name "*.dtb" -o -name "*.dtbo" | head -n 1)
-            if [ -n "$dtb_file" ]; then
-                cp "$dtb_file" "$ANYKERNEL_DIR/"
-                echo "Copied DTB/DTBO file: $dtb_file"
+    for dir in "${search_dirs[@]}"; do
+        for pattern in "${dtb_patterns[@]}"; do
+            local file=$(find "$KERNEL_DIR/$dir" -name "$pattern" -print -quit)
+            if [ -n "$file" ]; then
+                cp "$file" "$ANYKERNEL_DIR/"
+                echo "Copied DTB/DTBO: $file"
                 return 0
             fi
-        fi
+        done
     done
 
-    echo "No DTB/DTBO file found"
+    echo "No DTB/DTBO file found!"
     return 0  # Return 0 even if no file found, as it's optional
 }
 
 # Main execution
-copy_kernel_image
-copy_dtb_dtbo
+find_and_copy_kernel
+find_and_copy_dtb_dtbo
 
 # Create AnyKernel zip
 cd "$ANYKERNEL_DIR"
-zip -r9 "$FINAL_KERNEL_ZIP" * -x .git README.md kernel_zipper.sh *placeholder
-
-mv $FINAL_KERNEL_ZIP ../$FINAL_KERNEL_ZIP
+zip -r9 "$FINAL_KERNEL_ZIP" * -x .git README.md *placeholder
 
 echo "AnyKernel zip created: $FINAL_KERNEL_ZIP"
