25ffadda235b6ab18c3f321db66ccbdef78fd125
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2024-09-10 17:29:29 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -29,8 +29,8 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-BLOCK=auto;
-IS_SLOT_DEVICE=auto;
+BLOCK=dev/block/platform/bootdevice/by-name/boot;
+IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
 
