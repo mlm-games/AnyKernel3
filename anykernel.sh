7bf7baea484c2ec8d31ad6e0c5d074f620c4bdf1
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2025-01-30 09:13:54 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Rusk Kernel by ragebreaker
+kernel.string=My Packaged Kernel
 do.devicecheck=0
 do.modules=0
 do.systemless=1
